c2dbb42eaa2d8aba6983fad4d0127c0157bb5a53
unknown <593152@01HW346354.India.TCS.com>
2015-08-25 17:57:25 +0530

    Adding Comparator example program from Collections to JavaRepo

---
+++ src/com/somnath/collectionstrials/NameComparator.java
@@ -0,0 +1,17 @@
+package com.somnath.collectionstrials;
+
+import java.util.Comparator;
+
+public class NameComparator implements Comparator<Student> {
+	
+	@Override
+	public int compare(Student o1, Student o2){
+		String name1 = o1.getName();
+		String name2 = o2.getName();
+		
+		//ascending order (descending order would be: name2.compareTo(name1))
+		return name1.compareTo(name2);
+	}
+
+	
+}
